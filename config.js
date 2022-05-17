e67d59dfd8fde232f067e79c27b0b224bc53fe12
MOONKIGHT <102457400+MOONKIGHT@users.noreply.github.com>
2022-05-17 10:55:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.owner = JSON.parse(fs.readFileSync('./src/owner.json')) // Change owner n
 global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium user has unlimited limit 
 
-global.repox = 'raselcomel/mymd'
+global.repox = 'MOONKIGHT/Nrwwfcv'
 global.branx = 'master'
 
 global.APIs = { // API Prefix
@@ -45,8 +45,8 @@ global.APIKeys = { // APIKey Here
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = ''
-  var sticker_author = '               ig\n🔥 @rasel.ganz 🔥'
+  var sticker_name = 'IMASH'
+  var sticker_author = '⃧ᴍ⃥⃧͢🌑⃥⃭⃧🌕̶̷⃮⃧ɴ ⃥ᴋ⃯⃗ɴ⃧ɪ⃬⃗ɢ⃧ʜ⃯⃗ᴛ ʙ⃔⃧ᴏ⃕⃧ᴛ⃕-ᴢ⃕🌙'
  } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -67,8 +67,8 @@ global.author = sticker_author
 const cpack = fs.readFileSync("lib/colong.json")
 const ctickerpack = JSON.parse(cpack)
 if (ctickerpack.cpackname == '') {
-  var colong_name = 'sikilinsut olang nylong stikell'
-  var colong_author = 'Ciss 📸\n\n@rasel.ganz'
+  var colong_name = 'TEST NOW1'
+  var colong_author = 'TEST NOW2'
  } else {
   var colong_name = ctickerpack.cpackname
   var colong_author = ctickerpack.cauthor
@@ -85,33 +85,33 @@ global.pclg = colong_name
 global.aclg = colong_author
 //======================================//
 
-global.wm = '@rasel.ganz'
+global.wm = 'TEST NOW WM'
 global.ucpn = ''
 
 //================= Url =================//
-global.linkgc = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
-global.linkig = 'https://www.instagram.com/p/CH1A1c9J5pY/?utm_medium=copy_link'
-global.linkyt = ''
-global.linkfb = 'https://fb.watch/aYv0jAffAO'
-global.web = 'https://raselcomel.github.io'
-global.web1 = 'https://anu.rasell.repl.co'
+global.linkgc = 'https://chat.whatsapp.com/LebNBnyIOCR7vsTfeLutz7'
+global.linkig = 'https://youtu.be/InUJUVAD7bA'
+global.linkyt = 'https://youtu.be/InUJUVAD7bA'
+global.linkfb = 'https://www.facebook.com/nidusha.fernando'
+global.web = 'https://chat.whatsapp.com/LebNBnyIOCR7vsTfeLutz7'
+global.web1 = 'https://chat.whatsapp.com/LebNBnyIOCR7vsTfeLutz7'
 
 global.deslink = ''
-global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@rasel.ganz'
+global.titlink = 'Folllow me on facebook'
+global.bodlink = '©MOON KNIGHT BOT~Z'
 
 //========= Url Template Button =========//
-global.dtu = 'Group Bot WhatsApp'
-global.urlnya = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
+global.dtu = 'Group WhatsApp'
+global.urlnya = 'https://chat.whatsapp.com/LebNBnyIOCR7vsTfeLutz7'
 
 //========= Call Template Button =========//
 global.dtc = 'Add Me'
-global.nmbrnya = '+62 822-5604-8971'
+global.nmbrnya = '+94771323434'
 
 
 //============ Proses Bot ============//
-global.wait = 'Tunggu sebentar ya sayang'
-global.eror = 'Maaf terjadi kesalahan, silahkan coba lagi nanti'
+global.wait = 'WAIT'
+global.eror = 'ERORR'
 
 
 //========== Tampilan Bot ===========//
@@ -132,7 +132,7 @@ global.fsx = 999999999999  //fake size foto/video
 global.fsdx = 9999999999999  //fake size document 
 global.pcdx = 999999999999  // fake page count document
 
-global.versibot = '^0.0.4'
+global.versibot = '^0.0.7'
 
 global.multiplier = 69 // The higher, The harder levelup
 
